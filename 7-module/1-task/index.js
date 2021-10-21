dee6182ed3948ad36f367d12e0ff34f7f0316184
Alexander Shchukarev <aashchukarev@yandex.ru>
2021-10-21 15:18:45 +0300

    module 7 tasks 1 and 2 completed

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,54 @@
 import createElement from '../../assets/lib/create-element.js';
+import arrows from './arrows.js';
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    const ribbon = createElement(`<div class="ribbon">
+      <button class="ribbon__arrow ribbon__arrow_left">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+      <nav class="ribbon__inner"></nav>
+    </div>`);
+    const ribbonInner = ribbon.querySelector('.ribbon__inner');
+    let allRibbon = createElement(`<a href="#" class="ribbon__item ribbon__item_active" data-id="${categories[0].id}">${categories[0].name}</a>`);
+    ribbonInner.append(allRibbon);
+    let counter = categories.length;
+    for( let i = 1; i < counter; i++ ) {
+      let ribbonCategory = createElement(`<a href="#" class="ribbon__item" data-id="${categories[i].id}">${categories[i].name}</a>`)
+      ribbonInner.append(ribbonCategory);
+    }
+    ribbon.append(createElement(`<button class="ribbon__arrow ribbon__arrow_right ">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>`));
+
+    const clicker = arrows(ribbon);
+    if( clicker ) clicker();
+    this._elem = ribbon;
+    ribbonInner.addEventListener('click', this.select);
+  } 
+
+  select = (event) => {
+    event.preventDefault();
+    let target = event.target;
+    let ribbonInner = target.closest('.ribbon__inner');
+    let allCategories = ribbonInner.querySelectorAll('.ribbon__item');
+    for( let category of allCategories ) {
+      if( category.classList.contains('ribbon__item_active') ) category.classList.remove('ribbon__item_active');
+    }
+
+    if( target.closest('.ribbon__item') ) {
+      target.classList.add('ribbon__item_active');
+      let id = target.closest('[data-id]').dataset.id;
+      let myEvent = new CustomEvent('ribbon-select', {
+        detail:  id, 
+        bubbles: true
+      });
+      this._elem.dispatchEvent(myEvent);
+  }
+}
+
+  get elem() {
+    return this._elem;
   }
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,54 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    const modalWindow = createElement(`<div class="modal">
+      <div class="modal__overlay"></div>
+        <div class="modal__inner">
+        <div class="modal__header">
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+        <h3 class="modal__title"></h3>
+        </div>
+        <div class="modal__body"></div>
+        </div>
+      </div>`);
+
+    this._elem = modalWindow;
+    let button = this._elem.querySelector('.modal__close');
+    button.addEventListener('click', this.close);
+    document.addEventListener('keydown', this.kclose);
+  }
+
+  open() {
+    document.body.append(this._elem);
+    document.body.classList.add('is-modal-open');
+  }
+
+  setTitle(title) {
+    let modalTitle = this._elem.querySelector('.modal__title');
+    modalTitle.textContent = title;
+  }
+
+  setBody(node) {
+    let modalBody = this._elem.querySelector('.modal__body');
+    modalBody.append(node);
+  }
+
+  close() {
+    document.body.classList.remove('is-modal-open');
+    document.body.remove(this._elem);
+    document.removeEventListener('keydown', this.kclose);
+  }
+
+  kclose = (event) => {
+    if( event.code === 'Escape') {
+      event.preventDefault();
+      this.close();
+    }
+  }
+
+  get elem() {
+    return this._elem;
   }
 }
