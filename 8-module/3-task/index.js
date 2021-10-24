7ff79ab997a95d37ce1c59915437c078e423f599
Alexander Shchukarev <aashchukarev@yandex.ru>
2021-10-24 21:04:45 +0300

    Module 8 completed at 75%

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,37 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if( !product || product === null ) return;
+   
+    let rezult = this.cartItems.find( item => item.product.id == product.id );
+    if( rezult ) rezult.count += 1;
+    else this.cartItems.push({product: product, count: 1});
+
+    this.onProductUpdate(this.cartItems);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let findProduct = this.cartItems.find( item => item.product.id == productId );
+    let index = this.cartItems.indexOf(findProduct);
+    if( findProduct.count == 1 && amount == -1 ) this.cartItems.splice(index, 1);
+    else findProduct.count += amount;
+    
+    this.onProductUpdate(this.cartItems);
   }
 
   isEmpty() {
-    // ваш код
+    if( this.cartItems.length != 0 ) return false;
+    else return true;
   }
 
   getTotalCount() {
-    // ваш код
+    let totalCount = this.cartItems.reduce((sum, current) => sum + current.count, 0);
+    return totalCount;
   }
 
   getTotalPrice() {
-    // ваш код
+    let totalPrice = this.cartItems.reduce((sum, current) => sum + current.product.price * current.count, 0);
+    return totalPrice;
   }
 
   onProductUpdate(cartItem) {
